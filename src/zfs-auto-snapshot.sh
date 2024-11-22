0339799895525e1f67921f80deeea067b812c0e9
zpin <zpin@users.noreply.github.com>
2024-11-22 09:40:32 +0100

    fixed opt_verbose

--- src/zfs-auto-snapshot.sh
+++ src/zfs-auto-snapshot.sh
@@ -174,7 +174,7 @@ do_snapshots () # properties, flags, snapname, oldglob, [targets...]
 		if echo "$SNAPSHOTS_OLD" | grep -q "$ii@$NAME"
 		then
 			name_collision_skip=1
-			if [ $opt_verbose -gt 0 ]
+			if [ -n "$opt_verbose" ]
 			then
 				echo "Skipping target $ii, $ii@$NAME snapshot already exists."
 			fi
@@ -189,7 +189,7 @@ do_snapshots () # properties, flags, snapname, oldglob, [targets...]
 			if [ "$kb_written" -lt "$opt_min_size" ]
 			then
 				size_check_skip=1
-				if [ $opt_verbose -gt 0 ]
+				if [ -n "$opt_verbose" ]
 				then
 					echo "Skipping target $ii, only $kb_written kB written since last snap. opt_min_size is $opt_min_size kB."
 				fi
@@ -202,7 +202,7 @@ do_snapshots () # properties, flags, snapname, oldglob, [targets...]
 			if [ "$bytes_written" -eq 0 ]
 			then
 				size_check_skip=1
-				if [ $opt_verbose -gt 0 ]
+				if [ -n "$opt_verbose" ]
 				then
 					echo "Skipping target $ii, 0 bytes written since last snap."
 				fi
